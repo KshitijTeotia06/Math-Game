7f54a7209511a6182ec872ca87d955355212ab85
Kshitij T <teotia.kshitij@gmail.com>
2020-01-05 23:34:14 -0800

    inital commit

---
+++ HelloWorld/HelloWorld.py
@@ -0,0 +1,25 @@
+print('Hello World')
+
+#This is a comment
+"""
+This is a comment 
+It continues 
+Hello
+"""
+
+def HelloWorld():
+    print("HelloWorld")
+HelloWorld()
+
+def Greeting(name):
+    print("Hi " + name + "!")
+Greeting("Kshitij")
+
+def add(a,b):
+    print(a + b)
+add(10,15)
+
+def returnAdd(a,b):
+    return a + b
+sum = returnAdd(12,34)
+print(sum)
